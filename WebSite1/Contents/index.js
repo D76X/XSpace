d7e0814ecc66a456cc72b25b37946dea13a9549b
Davide Spano <davide.spano@wuerth-phoenix.com>
2021-04-17 15:07:38 +0200

    simple changes to _index.html/js to allow dynamic loading of fragments of html and insertions intop the document with jquery

--- WebSite1/Contents/index.js
+++ WebSite1/Contents/index.js
@@ -1,2 +1,3 @@
 // function showAlert() { window.alert("This is an alert!"); }
 // function changeStyle() { document.getElementById("header1").style.color = "red"; }
+$('#msgDiv').load('/article1.html');
