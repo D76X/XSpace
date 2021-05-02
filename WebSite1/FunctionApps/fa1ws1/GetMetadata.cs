0c5330a16a9b7bcfacad1aef9ff667945d96a3b4
Davide Spano <davide.spano@wuerth-phoenix.com>
2021-05-02 12:49:53 +0200

    added GetMetadata function simple prototype

---
+++ WebSite1/FunctionApps/fa1ws1/GetMetadata.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace fa1ws1
+{
+    public static class GetMetadata
+    {
+        [FunctionName("GetMetadata")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+                log.LogInformation("C# HTTP trigger GetMetadata");
+                // do something here!
+                log.LogInformation("C# HTTP trigger GetMetadata processed the request.");
+                return new OkObjectResult("C# HTTP trigger GetMetadata returned value!");
+        }
+    }
+
+    public class Metadata
+    {
+        public string Type {get; set;}
+        public string Name {get; set;}
+        public string Value {get; set;}
+    }
+}
+
+
